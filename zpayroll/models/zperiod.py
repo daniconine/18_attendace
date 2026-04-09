ac4e7788d3b6394b49798378ea239969e29ce134
root <84111424+daniconine@users.noreply.github.com>
2026-04-08 19:12:06 -0500

    Se termino funcionaldiad de zperiod para entrada de dias ,horas y otros a planilla

--- zpayroll/models/zperiod.py
+++ zpayroll/models/zperiod.py
@@ -55,16 +55,139 @@ class ZPeriod(models.Model):
                 'struct_id': salary_structure.id,
             })
 
+            ##########Bloque de ingreso de Insumos
+            #####Regla ASistencias
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Asistencias',
+                'code': 'WORK100',
+                'number_of_days': period.days_attended or 0.0,
+                'number_of_hours': 0.0,
+                'contract_id': contract.id,})
+            
+            #####Regla Ausencias/PErmiso
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Ausencias/Permisos',
+                'code': 'PERMISO',
+                'number_of_days': period.days_permissions or 0.0,
+                'number_of_hours': 0.0,
+                'contract_id': contract.id,})
+            
+            #####Regla Vacaciones
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Vacaciones Devengadas',
+                'code': 'VAC',
+                'number_of_days': period.days_vacations or 0.0,
+                'number_of_hours': 0.0,
+                'contract_id': contract.id,})
+            
+            #####Regla Inasistencia
             self.env['hr.payslip.worked.days'].create({
                 'payslip_id': payslip.id,
                 'name': 'Inasistencias',
-                'code': 'UNATTENDED',
+                'code': 'FALTAS',
                 'number_of_days': period.days_unattended or 0.0,
-                'number_of_hours': (period.days_unattended or 0.0) * 8,
-                'contract_id': contract.id,
-            })
+                'number_of_hours': 0.0,
+                'contract_id': contract.id,})
+            
+            #####Horas Extras al 25%
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Horas Extras 25%',
+                'code': 'hrs_25',
+                'number_of_days': 0.0,
+                'number_of_hours': period.hrs_25 or 0.0,
+                'contract_id': contract.id,})
+            
+            #####Horas Extras al 35%
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Horas Extras 35%',
+                'code': 'hrs_35',
+                'number_of_days': 0.0,
+                'number_of_hours': period.hrs_35 or 0.0,
+                'contract_id': contract.id,})
 
-            payslip.action_compute_sheet()
+            #####Horas Extras al 100%
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Horas Extras 100%',
+                'code': 'hrs_100',
+                'number_of_days': 0.0,
+                'number_of_hours': period.hrs_100 or 0.0,
+                'contract_id': contract.id,})
+            
+            #####Horas Extras al 200%
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Horas Extras 200%',
+                'code': 'hrs_200',
+                'number_of_days': 0.0,
+                'number_of_hours': period.hrs_200 or 0.0,
+                'contract_id': contract.id,})
+            
+            # Calculamos la conversión de minutos a horas
+            horas_tardanza = period.late_min_total / 60.0 if period.late_min_total else 0.0
+
+            ###########tardanzas
+            self.env['hr.payslip.worked.days'].create({
+                'payslip_id': payslip.id,
+                'name': 'Tardanzas',
+                'code': 'TARDE',
+                'number_of_days': 0.0,
+                'number_of_hours': horas_tardanza,
+                'contract_id': contract.id,})
+            
+            
+            ###otras Entradas
+            ###########Bonos con estrcutura para cada bono aparezca como una linea separada
+            """for bono in period.bonus_ids:
+                self.env['hr.payslip.input'].create({
+                    'payslip_id': payslip.id,
+                    'name': bono.note or 'Bono',
+                    'code': 'BONO',
+                    'amount': bono.amount,
+                    'contract_id': contract.id,
+                })"""
+                
+            ###########Bonos con estrcutura ideal para una sola linea   
+            # Sumamos el campo 'amount' de todos los registros en 'bonus_ids'
+            total_bonos = sum(period.bonus_ids.mapped('amount'))
+
+            #####Entrada de Bonos
+            self.env['hr.payslip.input'].create({
+                'payslip_id': payslip.id,
+                'name': 'Bonos del Periodo',
+                'code': 'BONO',                
+                'amount': total_bonos,
+                'contract_id': contract.id,})
+            
+            # Sumamos el campo 'amount' de todos los registros de comisiones
+            total_comisiones = sum(period.commission_ids.mapped('amount'))
+
+            #####Entrada de Bonos
+            self.env['hr.payslip.input'].create({
+                'payslip_id': payslip.id,
+                'name': 'Comisiones del Periodo',
+                'code': 'COMISION',                
+                'amount': total_comisiones,
+                'contract_id': contract.id,})
+            
+            # Sumamos el campo 'amount' de todos los registros de disctado de clases
+            total_dictado_clases = sum(period.class_line_ids.mapped('amount'))
+
+            #####Entrada de Bonos
+            self.env['hr.payslip.input'].create({
+                'payslip_id': payslip.id,
+                'name': 'Dictado de Clases del PeriodO',
+                'code': 'CLASES',                
+                'amount': total_dictado_clases,
+                'contract_id': contract.id,})
+            
+            
+            payslip.action_compute_sheet() #3ejecucion
 
             period.message_post(
                 body="Payslip creado con éxito para el empleado %s en el periodo %s - %s" % (
